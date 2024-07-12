9c036c5759bf2586245c162cd158ed9f0c786161
arthurvill <arthurvillareal925@gmail.com>
2024-07-12 17:15:14 +0900

    firstjs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Jose Nathaniel Ubas</h1>
+      <h2>Arthur Villareal</h2>
+    </div>
+  );
+}
+
+export default App;
